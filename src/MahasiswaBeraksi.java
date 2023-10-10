fae9c1c74822e0cc08f97824e92bfd33f505c635
Mzzqq <rezarifa661@gmail.com>
2023-10-10 19:51:39 +0700

    feat: create object called mhs

--- src/MahasiswaBeraksi.java
+++ src/MahasiswaBeraksi.java
@@ -1,5 +1,10 @@
 public class MahasiswaBeraksi {
     public static void main(String[] args) {
+        Mahasiswa mhs = new Mahasiswa();
+        mhs.nama = "Reza";
 
+        mhs.membaca();
+        mhs.nyontek();
+        mhs.modifikasi();
     }
 }
