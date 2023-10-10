9c0ba00984133738598d766c57de575cbe6455d3
Mzzqq <rezarifa661@gmail.com>
2023-10-10 19:59:54 +0700

    feat: add Nilai() method

--- src/Nilai.java
+++ src/Nilai.java
@@ -1,2 +1,17 @@
 public class Nilai {
+    String nama;
+    String NIM;
+    float nilaiAbsen;
+    float nilaiTugas;
+    float nilaiUts;
+    float nilaiUas;
+
+    float Nilai() {
+        float values = (float)
+                ((this.nilaiAbsen * 0.1) +
+                        (this.nilaiTugas * 0.2) +
+                        (this.nilaiUts * 0.3) +
+                        (this.nilaiUas * 0.4));
+        return values;
+    }
 }
