6f59fde3caf9854efb81d0d774c96b5b7fb7da51
Mzzqq <rezarifa661@gmail.com>
2023-10-10 20:16:40 +0700

    feat: add object called mhs

--- src/DemoNilai.java
+++ src/DemoNilai.java
@@ -1,5 +1,13 @@
 public class DemoNilai {
     public static void main(String[] args) {
+        Nilai mhs = new Nilai();
+        mhs.nama = "Rizky Fahrureza";
+        mhs.NIM = "2210631170149";
+        mhs.nilaiAbsen = 85.0f;
+        mhs.nilaiTugas = 80.8f;
+        mhs.nilaiUts = 90.0f;
+        mhs.nilaiUas = 93.0f;
 
+        mhs.CetakNilai();
     }
 }
