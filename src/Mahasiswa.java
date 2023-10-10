7b4183f548e88c50605c0f34d07ee75f141c7656
Mzzqq <rezarifa661@gmail.com>
2023-10-10 19:48:17 +0700

    Add Mahasiswa.java

---
+++ src/Mahasiswa.java
@@ -0,0 +1,14 @@
+public class Mahasiswa {
+    String nama;
+    void membaca() {
+        System.out.println(this.nama + " sedang membaca");
+    }
+
+    void nyontek() {
+        System.out.println(this.nama + " hobi menyontek");
+    }
+
+    void modifikasi() {
+        System.out.println(this.nama + " sedang memodifikasi nilai");
+    }
+}
