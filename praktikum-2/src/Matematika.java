721e7178f9d7a8ff50d4f7b72fd34574f9a0da64
reza <rezarifa662@gmail.com>
2023-10-18 17:03:18 +0700

    Initialize commit

---
+++ praktikum-2/src/Matematika.java
@@ -0,0 +1,30 @@
+public class Matematika {
+    int a, b;
+    public int pertambahan(int a, int b) {
+        this.a = a;
+        this.b = b;
+
+        return a + b;
+    }
+
+    public int pengurangan(int a, int b) {
+        this.a = a;
+        this.b = b;
+
+        return a - b;
+    }
+
+    public int perkalian(int a, int b) {
+        this.a = a;
+        this.b = b;
+
+        return a * b;
+    }
+
+    public double pembagian(int a, int b) {
+        this.a = a;
+        this.b = b;
+
+        return (double) a / b;
+    }
+}
