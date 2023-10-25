7bc3dbc67bb63f5930b9a1e40ac0f31a2258c9fa
Mzzqq <rezarifa661@gmail.com>
2023-10-25 09:54:23 +0700

    Inherit KonversiSuhu to KonversiSuhu2

---
+++ praktikum-3/src/praktikum/KonversiSuhu.java
@@ -0,0 +1,11 @@
+package praktikum;
+
+public class KonversiSuhu {
+    public static double celciusToFahrenheit(double celcius) {
+        return celcius * 9 / 5 + 32;
+    }
+
+    public static double celciusToReamur(double celcius) {
+        return celcius * 4 / 5;
+    }
+}
---
+++ praktikum-3/src/praktikum/KonversiSuhu2.java
@@ -0,0 +1,7 @@
+package praktikum;
+
+public class KonversiSuhu2 extends KonversiSuhu{
+    public static double fahrenheitToReamur(double fahrenheit) {
+        return (fahrenheit - 32) * 4 / 9;
+    }
+}
