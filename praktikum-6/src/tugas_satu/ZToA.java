683e8d99a93df35421492f32b5188424f2436a87
Mzzqq <rezarifa661@gmail.com>
2023-11-21 20:27:41 +0700

    add feature Z to A

--- praktikum-6/src/tugas_satu/ZToA.java
+++ praktikum-6/src/tugas_satu/ZToA.java
@@ -1,5 +1,10 @@
 package tugas_satu;
 
 public class ZToA {
-
+    public static void main(String[] args) {
+        System.out.println("Huruf Z - A");
+        for (char i = 'Z'; i >= 'A'; i--){
+            System.out.println(i);
+        }
+    }
 }
