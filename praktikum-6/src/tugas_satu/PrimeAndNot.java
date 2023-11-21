431086f9e268d50bc1d99d3c51f2f7e11b6f56ae
Mzzqq <rezarifa661@gmail.com>
2023-11-21 20:23:33 +0700

    Some changes at looping variable

--- praktikum-6/src/tugas_satu/PrimeAndNot.java
+++ praktikum-6/src/tugas_satu/PrimeAndNot.java
@@ -4,13 +4,11 @@ import java.util.Scanner;
 
 public class PrimeAndNot {
     public static void main(String[] args) {
-        int i, j;
-
         System.out.println("Deret bilangan prima dan bukan dari 0 - 20");
 
-        for (i = 0; i <= 20; i++){
+        for (int i = 0; i <= 20; i++){
             int factor = 0;
-            for (j = 1; j <= i; j++) {
+            for (int j = 1; j <= i; j++) {
                 if (i % j == 0){
                     factor++;
                 }
