bf6b70d977da4da3d61c9db30a2fad971827c3e2
Mzzqq <rezarifa661@gmail.com>
2023-11-21 20:29:39 +0700

    add input anak ayam

--- praktikum-6/src/tugas_satu/AnakAyam.java
+++ praktikum-6/src/tugas_satu/AnakAyam.java
@@ -1,4 +1,11 @@
 package tugas_satu;
 
+import java.util.Scanner;
+
 public class AnakAyam {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Masukkan input angka anak ayam: ");
+        int counter = scanner.nextInt();
+    }
 }
