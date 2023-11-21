3edacd502fc31acdb1095ba4f148f74d973a78f6
Mzzqq <rezarifa661@gmail.com>
2023-11-21 20:24:50 +0700

    add feature odd and even number

--- praktikum-6/src/tugas_satu/GanjilGenap.java
+++ praktikum-6/src/tugas_satu/GanjilGenap.java
@@ -1,4 +1,14 @@
 package tugas_satu;
 
 public class GanjilGenap {
+    public static void main(String[] args) {
+        System.out.println("Deret bilangan ganjil dan genap dari 0 - 20");
+        for (int i = 0; i <= 20; i++) {
+            if (i % 2 == 0) {
+                System.out.println("Bilangan genap: " + i);
+            } else {
+                System.out.println("Bilangan ganjil: " + i);
+            }
+        }
+    }
 }
