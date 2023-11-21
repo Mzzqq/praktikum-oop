6793cc2b9667296bbe54669861b736c22cfb6aab
Mzzqq <rezarifa661@gmail.com>
2023-11-21 20:31:56 +0700

    remove unused package

--- praktikum-6/src/tugas_satu/PrimeAndNot.java
+++ praktikum-6/src/tugas_satu/PrimeAndNot.java
@@ -1,7 +1,5 @@
 package tugas_satu;
 
-import java.util.Scanner;
-
 public class PrimeAndNot {
     public static void main(String[] args) {
         System.out.println("Deret bilangan prima dan bukan dari 0 - 20");
