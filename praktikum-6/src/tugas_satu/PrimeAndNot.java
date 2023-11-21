338fd8856e77197f31b9db73411f9b9a42699924
Mzzqq <rezarifa661@gmail.com>
2023-11-21 20:18:40 +0700

    add deret bilangan prima dan bukan

--- praktikum-6/src/tugas_satu/PrimeAndNot.java
+++ praktikum-6/src/tugas_satu/PrimeAndNot.java
@@ -4,6 +4,23 @@ import java.util.Scanner;
 
 public class PrimeAndNot {
     public static void main(String[] args) {
+        int i, j;
 
+        System.out.println("Deret bilangan prima dan bukan dari 0 - 20");
+
+        for (i = 0; i <= 20; i++){
+            int factor = 0;
+            for (j = 1; j <= i; j++) {
+                if (i % j == 0){
+                    factor++;
+                }
+            }
+
+            if (factor == 2){
+                System.out.println("Bilangan Prima:" + i);
+            } else {
+                System.out.println("Bilangan Bukan Prima:" + i);
+            }
+        }
     }
 }
