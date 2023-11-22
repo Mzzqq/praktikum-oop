182758be6f4d7988997b2dc3bb8e38e9474ce50e
Mzzqq <rezarifa661@gmail.com>
2023-11-22 13:05:52 +0700

    feat: create table perkalian

--- praktikum-6/src/tugas_dua/TabelPerkalian.java
+++ praktikum-6/src/tugas_dua/TabelPerkalian.java
@@ -4,6 +4,18 @@ import java.util.Scanner;
 
 public class TabelPerkalian {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.print("Masukkan nilai tabel perkalian: ");
+        int batasan = scanner.nextInt();
+
+        if (batasan <= 10) {
+            for (int i = 1; i <= batasan; i++) {
+                for (int j = 1; j <= batasan; j++){
+                    System.out.print((j*i) + "\t");
+                }
+                System.out.println();
+            }
+        }
     }
 }
