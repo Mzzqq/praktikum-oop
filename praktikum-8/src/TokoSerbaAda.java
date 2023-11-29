1f2f7004cefae5df5f9d4a0db9a200cd1ecdcfc5
Mzzqq <rezarifa661@gmail.com>
2023-11-29 22:46:09 +0700

    Implemented item input loop

--- praktikum-8/src/TokoSerbaAda.java
+++ praktikum-8/src/TokoSerbaAda.java
@@ -17,5 +17,16 @@ public class TokoSerbaAda {
         int itemBarang = scanner.nextInt();
 
         jumlahBeli = new int[itemBarang]; // Initialize jumlahBeli
+
+        for (int i = 0; i < itemBarang; i++) {
+            // Input kode barang
+            System.out.print("Data ke-" + (i + 1) + ":\n");
+            System.out.print("Masukkan kode: ");
+            String kode = scanner.next();
+
+            // Input jumlah beli
+            System.out.print("Masukkan jumlah beli: ");
+            jumlahBeli[i] = scanner.nextInt();
+        }
     }
 }
