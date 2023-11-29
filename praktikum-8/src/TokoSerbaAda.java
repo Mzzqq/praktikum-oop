d19a05de1b053df0fe77d7234659e4d9cad7922b
Mzzqq <rezarifa661@gmail.com>
2023-11-29 22:43:34 +0700

    Added Scanner object for user input and initialized jumlahBeli array

--- praktikum-8/src/TokoSerbaAda.java
+++ praktikum-8/src/TokoSerbaAda.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class TokoSerbaAda {
     private static final String[] kodeBarang = {"a001", "a002", "a003"};
     private static final String[] namaBarang = {"Buku", "Pensil", "Pulpen"};
@@ -5,6 +7,15 @@ public class TokoSerbaAda {
     private static int[] jumlahBeli; // Jumlah yang di beli
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        // String kode = "";
+        // int jumlahBayar = 0;
+
+        // Input item barang
+        System.out.print("Masukkan item barang: ");
+        int itemBarang = scanner.nextInt();
+
+        jumlahBeli = new int[itemBarang]; // Initialize jumlahBeli
     }
 }
