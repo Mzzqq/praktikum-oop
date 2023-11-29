fe7cc509353f73515d5e2c215576dd98accedd54
Mzzqq <rezarifa661@gmail.com>
2023-11-29 23:10:02 +0700

    Revert "Added initial data for product codes, names, and prices."
    This reverts commit 7bc34d8e830414d97b4c07686d1f5a414b045881.

--- praktikum-8/src/TokoSerbaAda.java
+++ praktikum-8/src/TokoSerbaAda.java
@@ -1,11 +1,6 @@
 import java.util.Scanner;
 
 public class TokoSerbaAda {
-    private static final String[] kodeBarang = {"a001", "a002", "a003"};
-    private static final String[] namaBarang = {"Buku", "Pensil", "Pulpen"};
-    private static final int[] harga = {3000, 4000, 5000};
-    private static int[] jumlahBeli; // Jumlah yang di beli
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
